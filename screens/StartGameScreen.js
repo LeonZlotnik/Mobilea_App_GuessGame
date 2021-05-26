569d739f6a73bdb895093480af68f6c163ad4205
Leon Zlotnik <leonzlotnik@Leons-MacBook-Pro.local>
2021-05-25 22:47:38 -0500

    first commit

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,63 @@
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard } from 'react-native';
+
+import Card from '../components/Card';
+import Colors from '../constants/color';
+import Input from '../components/Input';
+
+const StartGameScreen = props =>{
+
+    const [enteredValue, setenteredValue] = useState('');
+
+    const numberInputHandler = (inputText) => {
+        setenteredValue(inputText.replace(/^0-9/g,''));
+    }
+
+    return (
+        <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss()}}>
+            <View style={styles.screen}>
+                <Text style={styles.title}>The Game Screen</Text>
+                <Card style={styles.inputContainer}>
+                    <Text>Select a Number</Text>
+                    <Input style={styles.input} onChangeText={numberInputHandler} value={enteredValue} blurOnSubmit autoCapitalize="none" autoCorrect={false} keyboardType="number-pad" maxLength="2" />
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}><Button title="Reset" onPress={() => {}} color={Colors.accent}/></View>
+                    <View style={styles.button}><Button title="Confirm" onPress={() => {}} color={Colors.primary}/></View>
+                </View>
+                </Card>
+            </View>
+        </TouchableWithoutFeedback>
+    )
+};
+
+const styles = StyleSheet.create({
+    screen:{
+        flex: 1,
+        padding: 10,
+        alignItems: 'center'
+    },
+    buttonContainer:{
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+      },
+    title:{
+        fontSize: 20,
+        marginVertical: 10
+    },
+    button:{
+        width:100
+    },
+    input:{
+        width: 50,
+        textAlign: 'center'
+    }
+});
+
+export default StartGameScreen;
